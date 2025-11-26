6e632143373767534d9e8a81e8f3ffe0f68241bf
Saul Torres <SaulTorres1605@yahoo.com>
2025-11-25 22:40:23 -0700

    Fixed upbuild error

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "C:/Espressif/v5.3.4/esp-idf/components/bootloader/subproject")
+  file(MAKE_DIRECTORY "C:/Espressif/v5.3.4/esp-idf/components/bootloader/subproject")
+endif()
+file(MAKE_DIRECTORY
+  "C:/Micro_2/Labs/Sender/build/bootloader"
+  "C:/Micro_2/Labs/Sender/build/bootloader-prefix"
+  "C:/Micro_2/Labs/Sender/build/bootloader-prefix/tmp"
+  "C:/Micro_2/Labs/Sender/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Micro_2/Labs/Sender/build/bootloader-prefix/src"
+  "C:/Micro_2/Labs/Sender/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Micro_2/Labs/Sender/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Micro_2/Labs/Sender/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
